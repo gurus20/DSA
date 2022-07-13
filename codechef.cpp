8f9df786e49c0cdf9ab0c0b3be0a61b0ec161e19
Gurdayal <guru.gps.20@gmail.com>
2022-07-13 22:07:40 +0530

    gfg

--- codechef.cpp
+++ codechef.cpp
@@ -1,57 +1,39 @@
+// { Driver Code Starts
 #include <bits/stdc++.h>
-#define lli long long int
-#include <algorithm>
-
 using namespace std;
 
-int check(){
-    return 1;
-}
+int *Rearrange(int *arr, int n);
 
-int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        int x, y, a, b;
-        cin >> x >> y >> a >> y;
+int main()
+{
+    // code
 
-        if (x == a && y == b){
-            cout << "YES\n";
-        }
-        else{
-            
-        }
+    string str;
+    cin >> str;
+    
+    cout << str.size();
+    
 
-    }
     return 0;
+} // } Driver Code Ends
+
+int *Rearrange(int *arr, int n)
+{
+    int* b = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+
+        if (arr[i] != -1)
+        {
+            // cout << i  << " " << arr[i] << endl;
+            if (arr[i] == i){
+                b[i] = arr[i];
+            }
+            else
+                b[arr[i]] = arr[i];
+        }
+        else
+        b[i] = -1;    
+    }
+    return b;
 }
-
-// int main(){
-//     int tc;
-//     cin >> tc;
-
-//     while(tc--){
-//         int t, n, sum;
-//         cin >> t >> n >> sum;
-
-//         lli itr = 0;
-//         int cases = t, base = 1;
-
-//         while(sum != 0){
-//             int d = sum / (n * base);
-//             int m = min(d, cases);
-
-//             itr = m * (lli)(pow(n, 2));
-
-//             base *= 10;
-//             cases -= d;   
-//             sum %= (n * base);
-//             n = 
-//         }
-        
-//         cout << itr << endl;
-
-//     }
-
-//     return 0;
-// }
